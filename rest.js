f97dcae95de7dbc24ced515d31fc9780de4330e8
Fernando Flores Garcia <fflores10@uabc.edu.mx>
2018-11-29 09:41:02 -0800

    asda

--- rest.js
+++ rest.js
@@ -14,34 +14,53 @@ function consultaMensajes() {
 }
 
 function onMensajesResponse(response) {
-  console.log(response)
   mensajes = response
-  filtraMensajes()
+  var mensajes_conversacion = filtraMensajes()
+  $('#contenedor-mensajes').empty()
+  llenaMensajes(mensajes_conversacion)
 }
 
 // Se declaran los usuarios
-var usuario_actual = 'fer'
-var usuario_destino = 'otrofer'
+var usuario_actual = 'felipe'
+var usuario_destino = 'francisco'
 
-function mandaMensaje(mensaje) {
-  objeto_mensaje = {
-    id_folio : "20161003",
-    emisor : usuario_actual,
-    receptor : usuario_destino,
-    mensaje : mensaje,
-    fecha_act : 20181113
+function mandaMensaje() {
+  let mensaje = $('#mensaje-input').val()
+  console.log(mensaje)
+  if(mensaje != '' && mensaje != undefined) {
+    objeto_mensaje = {
+      id_folio : "20161003",
+      emisor : usuario_actual,
+      receptor : usuario_destino,
+      mensaje : mensaje,
+      fecha_act : 20181113
+    }
+    url = 'https://transportacionessdi.herokuapp.com/mensajes'
+    $.post(url, objeto_mensaje, function(data, status) {
+      consultaMensajes()
+    })
   }
-  console.log(JSON.stringify(objeto_mensaje))
-  url = 'https://transportacionessdi.herokuapp.com/mensajes'
-  $.post(url, objeto_mensaje, function(data, status) {
-    consultaMensajes()
-  })
 }
 
-consultaMensajes()
+function llenaMensajes(mensajes_conversacion) {
+  for(var key in mensajes_conversacion) {
+    mensaje = mensajes_conversacion[key]
+    if(mensaje.emisor == usuario_destino) {
+      chat_bubble = '<div class="row"> <div class="col"><div class="alert alert-primary">' +mensaje.mensaje + '</div></div><div class="col"></div></div>'
+    } else {
+      chat_bubble = '<div class="row"><div class="col"></div> <div class="col"><div class="alert alert-primary">' +mensaje.mensaje + '</div></div></div>'
+    }
+    $('#contenedor-mensajes').append(chat_bubble)
+  }
+}
 
 function filtraMensajes() {
   var mensajes_filtrados = []
   mensajes_filtrados = mensajes.filter(mensaje => (mensaje.receptor === usuario_destino && mensaje.emisor === usuario_actual) || (mensaje.emisor === usuario_actual && mensaje.emisor === usuario_destino))
-  console.log(mensajes_filtrados)
+  return mensajes_filtrados
 }
+
+consultaMensajes()
+$(document).ready(function () {
+  $('#boton-enviar').click(mandaMensaje())
+})
